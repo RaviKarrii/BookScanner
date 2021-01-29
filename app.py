b882428507113ca9fa12dcfcd7da6562fb23c293
Ravi Shanker Reddy Karri <me@ravikarri.in>
2021-01-29 19:35:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,8 @@ def twopage():
     if request.method == 'POST':
         f = request.files.getlist("file")
         for i in f:
-            i.save(secure_filename(i.filename))
+            i.save(os.path.join("/tmp/",secure_filename(i.filename)))
     return 'file uploaded successfully'
 
 if __name__ == '__main__':
-    app.config['UPLOAD_FOLDER'] = "/tmp"
     app.run()
