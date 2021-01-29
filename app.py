4a820971692d13b2cb4561a8d308bc55c2cf46fd
Ravi Shanker Reddy Karri <me@ravikarri.in>
2021-01-29 19:16:57 +0530

    Fix code

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ def hello():
 @app.route('/twopage')
 def twopage():
     if request.method == 'POST':
-      f = request.files[getlist("file")
-      for i in f:
-        i.save(secure_filename(i.filename))
-      return 'file uploaded successfully'
+        f = request.files.getlist("file")
+        for i in f:
+            i.save(secure_filename(i.filename))
+    return 'file uploaded successfully'
 
 if __name__ == '__main__':
     app.config['UPLOAD_FOLDER'] = "/tmp/"
