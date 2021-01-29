2bc4daaa69532043100b9549ba07d169bc7b432b
Ravi Shanker Reddy Karri <me@ravikarri.in>
2021-01-29 19:12:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 app = Flask(__name__)
 
 
@@ -6,5 +7,14 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/twopage')
+def twopage():
+    if request.method == 'POST':
+      f = request.files[getlist("file")
+      for i in f:
+        i.save(secure_filename(i.filename))
+      return 'file uploaded successfully'
+
 if __name__ == '__main__':
+    app.config['UPLOAD_FOLDER'] = "/tmp/"
     app.run()
